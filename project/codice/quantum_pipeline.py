2d5cb30e513d92acc2cc828cac95b1b71abe1276
adriano22_ <califano993@gmail.com>
2023-06-20 14:15:11 +0200

    Major update
    Plotted classical gridsearch results
    Started testing on quantum pipeline

--- project/codice/quantum_pipeline.py
+++ project/codice/quantum_pipeline.py
@@ -1,30 +1,40 @@
-from lambeq import BobcatParser, Rewriter
+from lambeq import BobcatParser, Rewriter, SpacyTokeniser
 from lambeq import IQPAnsatz, AtomicType, remove_cups
 from pytket.extensions.qiskit import AerBackend
-from lambeq import TketModel, QuantumTrainer, SPSAOptimizer, Dataset
+from lambeq import NumpyModel, QuantumTrainer, SPSAOptimizer, Dataset
+from lambeq import BinaryCrossEntropyLoss
 from utilities import *
+import torchmetrics
 import os, warnings, numpy as np, matplotlib.pyplot as plt
 
 
 warnings.filterwarnings("ignore")
-os.environ["TOKENIZERS_PARALLELISM"] = "True"
+os.environ["TOKENIZERS_PARALLELISM"] = "False"
 
-backend = AerBackend()
-backend_config = {
-    'backend': backend,
-    'compilation': backend.default_compilation_pass(2),
-    'shots': 8192
-}
 
-loss = lambda y_hat, y: -np.sum(y * np.log(y_hat)) / len(y)
-acc = lambda y_hat, y: np.sum(np.round(y_hat) == y) / len(y) / 2
-eval_metrics = {"acc": acc}
+def accuracy(y_hat, y):
+    return torchmetrics.functional.accuracy(y_hat, y, "binary")
+
+def precision(y_hat, y):
+    return torchmetrics.functional.precision(y_hat, y, "binary")
+
+def recall(y_hat, y):
+    return torchmetrics.functional.recall(y_hat, y, "binary")
+
+def f1score(y_hat, y):
+    return torchmetrics.functional.f1_score(y_hat, y, "binary")
+
+def precision(y_hat, y):
+    return torchmetrics.functional.precision(y_hat, y, "binary")
+
+eval_metrics = {"acc": accuracy, "prec": precision, "rec": recall, "f1": f1score}
 
 
 class QuantumPipeline():
     SUPPORTED_RULES = ["auxiliary", "connector", "coordination", "curry", "determiner", "postadverb", "preadverb", "prepositional_phrase", "object_rel_pronoun", "subject_rel_pronoun"]
     
     def __init__(self, parser, ansatz) -> None:
+        self.__tokeniser = SpacyTokeniser()
         self.__parser = parser
         self.__ansatz = ansatz
         self.__rewriter = Rewriter()
@@ -32,39 +42,46 @@ class QuantumPipeline():
     def add_rewriter_rules(self, *rules) -> None:
         self.__rewriter.add_rules(*rules)
         
-    def create_circuits_and_labels(self, dataset: str, control = None):
-        labels, sentences = extract_data(dataset)
-        diagrams = self.__parser.sentences2diagrams(sentences, suppress_exceptions = True)
-
-        normalized_diagrams = [self.__rewriter(diagram).normal_form() for diagram in diagrams if diagram is not None]      
-        edited_labels = [label for (diagram, label) in zip(diagrams, labels) if diagram is not None]             
-        circuits = [self.__ansatz(remove_cups(diagram)) for diagram in normalized_diagrams]            
+    def create_circuits_and_labels(self, dataset: str):
+        labels, sentences = extract_data_manual(dataset)
+        lower_sentences = [s.lower() for s in sentences]
+        diagrams = self.__parser.sentences2diagrams(lower_sentences, tokenised = False)
+        
+        normalized_diagrams = [diagram.normal_form() for diagram in diagrams if diagram is not None]    
+        edited_labels = [label for (diagram, label) in zip(diagrams, labels) if diagram is not None]   
+                  
+        circuits = [self.__ansatz(remove_cups(diagram)) for diagram in normalized_diagrams]      
         
         return edited_labels, circuits
     
     def create_dataset(self, labels, circuits):
-        return Dataset(circuits, labels)
+        return Dataset(circuits, labels, shuffle = False)
     
-    def create_trainer(self, *circuits):
+    def create_model(self, *circuits):
         circuits_model = []
         for circuit in circuits:
             circuits_model = circuits_model + circuit
 
-        self.__model = TketModel.from_diagrams(circuits_model, backend_config = backend_config)
+        self.__model = NumpyModel.from_diagrams(circuits_model, use_jit = False)
+        return self.__model
+    
+    def create_trainer(self, model = None, n_epochs = 0, a_hyp = 0.05, seed = CLASSIC_SEED):
         self.__trainer = QuantumTrainer(
-            model = self.__model,
-            loss_function = loss,
-            epochs = 100,
+            model = model,
+            loss_function = BinaryCrossEntropyLoss(use_jax = True),
+            epochs = n_epochs,
             optimizer = SPSAOptimizer,
-            optim_hyperparams = {'a': 0.05, 'c': 0.06, 'A':0.01 * 1000},
+            optim_hyperparams = {'a': a_hyp, 'c': 0.06, 'A':0.01 * n_epochs},
             evaluate_functions = eval_metrics,
             evaluate_on_train = True,
             verbose = "text",
-            seed = CLASSIC_SEED
+            seed = seed
         )
         
-    def train_model(self, train_set, test_set):        
-        self.__trainer.fit(train_set, test_set, evaluation_step = 1, logging_step = 1)
+        return self.__trainer
+        
+    def train_model(self, train_set, test_set, eval_step, log_step):        
+        self.__trainer.fit(train_set, test_set, evaluation_step = eval_step, logging_step = log_step)
         
     def plot(self):
         fig1, ((ax_tl, ax_tr), (ax_bl, ax_br)) = plt.subplots(2, 2, sharex = True, sharey='row', figsize=(10, 6))
